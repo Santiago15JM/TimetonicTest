e56e030c895dbf28905f4df40827e8bb7407ddf5
Santiago15JM <santiago15jmarin@gmail.com>
2024-06-05 17:11:14 -0500

    fixed missing slash exception and removed unnecessary builder

--- app/src/main/java/com/sjm/timetonic/config/RetrofitHelper.kt
+++ app/src/main/java/com/sjm/timetonic/config/RetrofitHelper.kt
@@ -1,20 +1,20 @@
 package com.sjm.timetonic.config
 
-import com.google.gson.GsonBuilder
 import com.sjm.timetonic.logic.ApiDefinitions
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    private const val baseUrl = "https://timetonic.com/live/api.php"
+    private const val baseUrl = "https://timetonic.com/live/api.php/"
+
     fun getInstance(): ApiDefinitions {
         val client = OkHttpClient.Builder().build()
 
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .client(client)
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiDefinitions::class.java)
     }
