fb0b3518705df3a0cd8221b976274f6d4ed4c8bd
Santiago15JM <santiago15jmarin@gmail.com>
2024-06-05 17:08:45 -0500

    implemented api functions

---
+++ app/src/main/java/com/sjm/timetonic/logic/API.kt
@@ -0,0 +1,19 @@
+package com.sjm.timetonic.logic
+
+import com.sjm.timetonic.config.RetrofitHelper
+
+object API {
+    private val api = RetrofitHelper.getInstance()
+
+    suspend fun createAppKey(): AppKeyResponse? {
+        return api.createAppKey().body()
+    }
+
+    suspend fun createOauthkey(user: String, password: String, appKey: String): OauthKeyResponse? {
+        return api.createOauthkey(user = user, password = password, appKey = appKey).body()
+    }
+
+    suspend fun createSesskey(oauthUser: String, oauthkey: String): SessionKeyResponse? {
+        return api.createSesskey(oauthUser = oauthUser, oauthkey = oauthkey).body()
+    }
+}
