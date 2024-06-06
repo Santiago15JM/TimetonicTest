d1862ad8379cc37273d13540a77112f158d2581a
Santiago15JM <santiago15jmarin@gmail.com>
2024-06-06 12:33:03 -0500

    added user functions to Preferences and dataStore name

--- app/src/main/java/com/sjm/timetonic/logic/Preferences.kt
+++ app/src/main/java/com/sjm/timetonic/logic/Preferences.kt
@@ -8,10 +8,12 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore("")
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore("session-details")
 
 object Preferences {
     private const val SESSION_PREFERENCES_KEY = "session"
+    private const val USER_PREFERENCES_KEY = "user"
+
     suspend fun saveSessionToken(ctx: Context, token: String) {
         ctx.dataStore.edit { prefs ->
             prefs[stringPreferencesKey(SESSION_PREFERENCES_KEY)] = token
@@ -21,4 +23,12 @@ object Preferences {
     suspend fun getSessionToken(ctx: Context): String? =
         ctx.dataStore.data.first()[stringPreferencesKey(SESSION_PREFERENCES_KEY)]
 
+    suspend fun saveUser(ctx: Context, token: String) {
+        ctx.dataStore.edit { prefs ->
+            prefs[stringPreferencesKey(USER_PREFERENCES_KEY)] = token
+        }
+    }
+
+    suspend fun getUser(ctx: Context): String? =
+        ctx.dataStore.data.first()[stringPreferencesKey(USER_PREFERENCES_KEY)]
 }
